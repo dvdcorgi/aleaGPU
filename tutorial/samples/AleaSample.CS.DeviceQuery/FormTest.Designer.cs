77a6d999ab7ca0423a9dbfab5e71abf3708795d5
Dasumi <Dasumi@CORGIPC>
2018-01-31 21:43:08 +0800

    Initial Commit

---
+++ tutorial/samples/AleaSample.CS.DeviceQuery/FormTest.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DeviceQuery
+{
+    partial class FormTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(63, 22);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(117, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Run Transform";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 67);
+            this.Controls.Add(this.btnTest);
+            this.Name = "FormTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dat Alea Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+    }
+}
---
+++ tutorial/samples/AleaSample.CS.DeviceQuery/FormTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alea;
+using Alea.CSharp;
+using NUnit.Framework;
+
+namespace DeviceQuery
+{
+    public partial class FormTest : Form
+    {
+        public FormTest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = Gpu.Default;
+
+            Run();
+        }
+
+        private const int Length = 1000000;
+
+        private static void Kernel(int[] result, int[] arg1, int[] arg2)
+        {
+            var start = blockIdx.x * blockDim.x + threadIdx.x;
+            var stride = gridDim.x * blockDim.x;
+            for (var i = start; i < result.Length; i += stride)
+            {
+                result[i] = arg1[i] + arg2[i];
+            }
+        }
+
+        [GpuManaged, Test]
+        public static void Run()
+        {
+            var gpu = Gpu.Default;
+
+            Console.WriteLine("Dat GPU: " + gpu);
+
+            var lp = new LaunchParam(16, 256);
+            var arg1 = Enumerable.Range(0, Length).ToArray();
+            var arg2 = Enumerable.Range(0, Length).ToArray();
+            var result = new int[Length];
+
+            gpu.Launch(Kernel, lp, result, arg1, arg2);
+
+            var expected = arg1.Zip(arg2, (x, y) => x + y);
+
+            Assert.That(result, Is.EqualTo(expected));
+
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Console.WriteLine(result.GetValue(i));
+            }
+        }
+    }
+}
